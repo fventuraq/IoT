d63689efa26d29f3912733aca9c49ceab1026cef
FRANKLIN <github@midiacom.uff.br>
2020-05-20 15:41:45 -0300

    solucion del bug RESTCONTROLLER

--- src/main/java/com/franklin/pi4led/controller/LedController.java
+++ src/main/java/com/franklin/pi4led/controller/LedController.java
@@ -2,17 +2,19 @@ package com.franklin.pi4led.controller;
 
 import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController //ADD SERVIS REST FOR CONTROLLER
 public class LedController {
 
     private static GpioPinDigitalOutput pin;
 
-    @RequestMapping("/")
+    @RequestMapping("/") //PUERTO SIN APLICATION
     public String greeting(){
         return "Welcome Franklin";
     }
 
-    @RequestMapping("/light")
+    @RequestMapping("/light") //IP + PORT + /light
     public String light(){
         if(pin == null){
             GpioController gpio = GpioFactory.getInstance();
